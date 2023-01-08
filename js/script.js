2b9d01501ca5fbdff16e6ae531765998c190446c
Filkov Nikolay Lvovich <54195015+Dathc@users.noreply.github.com>
2023-01-08 22:45:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -510,7 +510,7 @@ function handleOrientation(e) {
   if (firstMove) {
     lastUD = e.beta;
     lastLR = e.gamma;
-    firstMove = true;//false
+    firstMove = false;
   }
   if (allowTilt) {
     if (e.beta < lastUD - mThreshold) {
